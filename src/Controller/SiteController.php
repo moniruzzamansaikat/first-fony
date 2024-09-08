a4cde8609703899e8bf39bc744d905607fb8278b
moniruzzaman.saikat <saikat.moniruzzaman@gmail.com>
2024-09-08 21:08:08 +0600

    first con & view

---
+++ src/Controller/SiteController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Kernel;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class SiteController extends AbstractController
+{
+    #[Route('/site', name: 'app_site')]
+    public function index(): Response
+    {
+        return $this->render('site/index.html.twig', [
+            'symfony_version' => Kernel::VERSION,
+            'php_version' => PHP_VERSION,
+            'controller_name' => 'SiteController',
+        ]);
+    }
+}
